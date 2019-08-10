70c35ab1f25d6db7191d80221d50ca03d0fba4f6
Ark Wu <arkw@live.com>
2019-08-11 00:32:54 +0800

    Updates

---
+++ precache-manifest.4510b66dc07932bda162c0c58a828e28.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "180327f3c1c6a7601060f4b9cb069d8e",
+    "url": "/user-rolodex/index.html"
+  },
+  {
+    "revision": "56c11b20ff63e919fa60",
+    "url": "/user-rolodex/static/css/main.3a9ab59e.chunk.css"
+  },
+  {
+    "revision": "90bc354edad42df017e0",
+    "url": "/user-rolodex/static/js/2.08bb4953.chunk.js"
+  },
+  {
+    "revision": "56c11b20ff63e919fa60",
+    "url": "/user-rolodex/static/js/main.10106309.chunk.js"
+  },
+  {
+    "revision": "3c4f677d2b487fd6fb16",
+    "url": "/user-rolodex/static/js/runtime~main.ddaf32c9.js"
+  }
+]);
